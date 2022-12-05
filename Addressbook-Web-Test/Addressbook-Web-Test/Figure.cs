a83b26746bac5553a956fd88040f0963502c156a
nas <nas@ASUS1>
2022-12-05 22:06:17 +0300

    Creat classes

---
+++ Addressbook-Web-Test/Addressbook-Web-Test/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook_Web_Test
+{
+    internal class Figure
+    {
+        private bool colored = false;
+        public bool Colered
+        {
+            get
+            {
+                return colored;
+            }
+            set
+            {
+                colored = value;
+            }
+
+        }
+    }
+}
