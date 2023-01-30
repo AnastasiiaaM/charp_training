41e5f2edb890465ac767ee2a56fcb87d75939291
nas <nas@ASUS1>
2023-01-31 00:26:20 +0300

    Adding tests White

---
+++ addressbook_tests_white/addressbook_tests_white/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_white")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_white")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f7b5ec6-65cb-47f5-a316-8879b813a93d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_white/addressbook_tests_white/appmanager/GroupHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestStack.White.InputDevices;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.WindowsAPI;
+using System.Windows.Automation;
+
+namespace addressbook_tests_white
+{
+    public class GroupHelper : HelperBase 
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> grouplist = new List<GroupData>();
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            foreach (TreeNode item in root.Nodes)
+            {
+                grouplist.Add(new GroupData()
+                {
+                    Name = item.Text
+                });
+            }
+            CloseGroupDialogue(dialogue);
+            return grouplist;
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            Window dialogue = OpenGroupsDialogue();
+
+            dialogue.Get<Button>("uxNewAddressButton").Click();
+            TextBox textBox = (TextBox) dialogue.Get(SearchCriteria.ByControlType(ControlType.Edit));
+            textBox.Enter(newGroup.Name);
+            Keyboard.Instance.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN);
+            CloseGroupDialogue(dialogue);
+
+        }
+
+        private void CloseGroupDialogue(Window dialogue)
+        {
+            dialogue.Get<Button>("uxCloseAddressButton").Click();
+        }
+
+        private Window OpenGroupsDialogue()
+        {
+            manager.MainWindow.Get<Button>("groupButton").Click();
+            return manager.MainWindow.ModalWindow(GROUPWINTITLE);
+        }
+    }
+}
